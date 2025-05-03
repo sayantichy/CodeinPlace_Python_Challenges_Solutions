4365988254e32fe72f178a0b7203855ffe19ef08
Sayanti Chowdhury <kuasha10102@gmail.com>
2025-05-03 22:46:43 +0600

    Create multiply2nums.py

---
+++ multiply2nums.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program multiplies two numbers.")
+    x=int(input("Enter first number: "))
+    y=int(input("Enter second number: "))
+    print(x*y)
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
